c6bab2bf5b5f67f9a270abafba1b454d80dc776f
sureshbabu44 <140206151+sureshbabu44@users.noreply.github.com>
2023-07-22 00:31:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,9 @@ streamlit.dataframe(fruityvice_normalized)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+import snowflake.connector
+
+
 
 
  
